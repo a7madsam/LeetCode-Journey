d8b9d1551bd0886b06d2172969a9bdd8b9b3b302
Ahmad Sameer <lolahmad987@gmail.com>
2022-09-30 18:04:53 +0300

    Time: 47 ms (7.76%), Space: 13.9 MB (26.19%) - LeetHub

---
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        if(nums.size()==1) return nums;
+        vector<pair<int,int>>freq;
+       map<int,int>m;
+        for(int i=0;i<nums.size();i++){
+            m[nums[i]]++;
+        }
+        for(auto it=m.begin();it!=m.end();it++){
+            freq.push_back(make_pair(it->second,it->first));
+        }
+        sort(freq.begin(),freq.end(),greater<pair<int,int>>());
+        vector<int> ans;
+        for(int i=0;i<k;i++){
+            ans.push_back(freq[i].second);
+        }
+        return ans;
+    }
+};
