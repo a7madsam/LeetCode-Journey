a205ff051fbade7557d9811074193ec28b9d8c62
Ahmad Sameer <lolahmad987@gmail.com>
2023-04-21 13:49:49 +0300

    Time: 10 ms (61.02%), Space: 7.2 MB (84.15%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        map<char,int> freq;
+        unordered_map<char,int> freq;
         if(s.length() != t.length())
         return false;
         for(int i=0;i<s.length();i++){
