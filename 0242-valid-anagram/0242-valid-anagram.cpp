5c900c0115ae6104d71744feef78edb1a11f8e05
Ahmad Sameer <lolahmad987@gmail.com>
2023-04-21 17:07:47 +0300

    Time: 8 ms (74.92%), Space: 7.3 MB (60.39%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,15 +1,16 @@
 class Solution {
+    int freq[26];
 public:
     bool isAnagram(string s, string t) {
         if(s.length() != t.length())
         return false;
-        int freq[26]={0};
+        int sum=0;
         for(int i=0;i<s.length();i++){
             freq[s[i]-'a']++;
             freq[t[i]-'a']--;
         }
         for(int i=0;i<26;i++){
-            if(freq[i])
+            if(freq[i]!=0)
             return false;
         }
         
