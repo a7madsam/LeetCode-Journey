b5f6391e45e22831b1b8d748430273f1d75ef65d
Ahmad Sameer <lolahmad987@gmail.com>
2023-04-22 08:48:49 -0700

    Time: 577 ms (28.40%), Space: 49.8 MB (58.61%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -4,21 +4,18 @@ public:
         if(!nums.size())
             return 0;
         unordered_map<int,int> m;
-        unordered_map<int,bool> visited;
         for(int i=0; i<nums.size(); i++) {
             m[nums[i]] = i;
         }
         int mx= 0;
         for(int j=0; j<nums.size(); j++) {
-            if(visited.find(m[nums[j]]) != visited.end() || m.find(nums[j]-1) != m.end()) {
+            if(m.find(nums[j]-1) != m.end()) {
                 continue;
             }
-            int begin = nums[j];
             int cnt = 0;
-            for(int i=begin;; i++) {
+            for(int i=nums[j];; i++) {
                 if(m.find(i)!=m.end()) {
                     cnt++;
-                    visited[m[i]] = true;
                 } else {
                     break;
                 }
